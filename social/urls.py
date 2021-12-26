c7117660b55011ed99ca8146bec1873416604840
islombeknematov <nematovislombek05@gmail.com>
2021-12-26 10:34:29 +0500

    new

--- social/urls.py
+++ social/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import PostModelListView, PostModelDetailView
+from .views import PostModelListView, PostModelDetailView, PostModelUpdateView, PostModelDeleteView
 
 app_name = 'social'
 
 urlpatterns = [
     path('', PostModelListView.as_view(), name='post-list'),
     path('post/<int:pk>/', PostModelDetailView.as_view(), name='post-detail'),
+    path('post/update/<int:pk>/', PostModelUpdateView.as_view(), name='post-update'),
+    path('post/delete/<int:pk>/', PostModelDeleteView.as_view(), name='post-delete'),
 ]
--- social/views.py
+++ social/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views import View
+from django.urls import reverse_lazy
 
 from .forms import PostModelForm, CommentModelForm
-from .models import PostModel
+from .models import PostModel, CommentModel
+from django.views.generic.edit import UpdateView, DeleteView
 
 
 class PostModelListView(View):
@@ -38,14 +40,48 @@ class PostModelDetailView(View):
     def get(self, request, pk, *args, **kwargs):
         post = PostModel.objects.get(pk=pk)
         form = CommentModelForm()
+        comments = CommentModel.objects.filter(post=post).order_by('-created_at')
 
         context = {
             'post': post,
             'form': form,
+            'comments': comments,
+        }
+
+        return render(request, 'social/post_detail.html', context)
+
+    def post(self, request, pk, *args, **kwargs):
+        post = PostModel.objects.get(pk=pk)
+        form = CommentModelForm(request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.author = request.user
+            new_comment.post = post
+            new_comment.save()
+
+        comments = CommentModel.objects.filter(post=post).order_by('-created_at')
+
+        context = {
+            'post': post,
+            'form': form,
+            'comments': comments,
+
         }
 
         return render(request, 'social/post_detail.html', context)
 
 
+class PostModelUpdateView(UpdateView):
+    model = PostModel
+    fields = ['body']
+    template_name = 'social/post_update.html'
+
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        return reverse_lazy('social:post-detail', kwargs={'pk': pk})
 
 
+class PostModelDeleteView(DeleteView):
+    model = PostModel
+    template_name = 'social/post_delete.html'
+    success_url = reverse_lazy('social:post-list')
