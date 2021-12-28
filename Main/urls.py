4cf842deba8166e688f1571d96b846f4f9ce677c
islombeknematov <nematov.islom.dev@gmail.com>
2021-12-29 00:46:34 +0500

    login

--- Main/urls.py
+++ Main/urls.py
@@ -1,14 +1,14 @@
-
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('accounts/', include('registration.backends.default.urls')),
+    # path('accounts/', include('registration.backends.default.urls')),
     path('admin/', admin.site.urls),
     path('', include('landing.urls', namespace='landing')),
     path('social/', include('social.urls', namespace='social')),
+    path('accounts/', include('allauth.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
